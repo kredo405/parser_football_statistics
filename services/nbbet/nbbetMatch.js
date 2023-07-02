4a8c8f657838a893dcabfe34559310e4c27060d5
Kredo <kredik305@gmail.com>
2023-07-02 22:34:58 +0300

    fix url

--- services/nbbet/nbbetMatch.js
+++ services/nbbet/nbbetMatch.js
@@ -1,33 +1,34 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const getMatchNbBetPrematch = async (link) => {
-    const desktop_agents = ['Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
-        'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14',
-        'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
-        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
-        'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
-        'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'];
+    const desktop_agents = [
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14",
+        "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0",
+    ];
 
     let rand = Math.floor(Math.random() * desktop_agents.length);
 
     const options = {
-        method: 'GET',
-        url: `https://app.nb-bet.com/v1/page-soccer-events/${link}/50/12/true/false/true/false/48/true/true`,
+        method: "GET",
+        url: `https://app.nb-bet.com/v1/soccer/events/page/${link}/50/12/true/false/true/false/true/true`,
         headers: {
-            'User-Agent': desktop_agents[rand],
-        }
+            "User-Agent": desktop_agents[rand],
+        },
     };
     try {
         const response = await axios.request(options);
         const result = await response.data;
 
         return result;
-    }
-    catch (error) {
+    } catch (error) {
         console.log(error);
     }
 };
--- test.js
+++ test.js
@@ -1,83 +1,28 @@
 import axios from "axios";
-import jsdom from "jsdom";
-const { JSDOM } = jsdom;
-const lineupsSoccer365 = async (link) => {
-    const desktop_agents = [
-        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36",
-        "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36",
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36",
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14",
-        "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36",
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36",
-        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
-        "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36",
-        "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0",
-    ];
 
-    let rand = Math.floor(Math.random() * desktop_agents.length);
-
+const getData = async () => {
     const options = {
-        method: "GET",
-        url: `https://www.excapper.com/?action=game&id=32383104`,
+        method: "POST",
+        url: "https://openai80.p.rapidapi.com/images/generations",
         headers: {
-            "User-Agent": desktop_agents[rand],
+            "content-type": "application/json",
+            "X-RapidAPI-Key":
+                "f570367049msh92d23c8fda1a817p1b03cfjsne8957d93c6e0",
+            "X-RapidAPI-Host": "openai80.p.rapidapi.com",
+        },
+        data: {
+            prompt: "A cute baby sea otter",
+            n: 2,
+            size: "1024x1024",
         },
     };
 
     try {
         const response = await axios.request(options);
-        const result = await response.data;
-
-        const outcomed = [];
-        const tabs = [];
-        const dom = new JSDOM(result);
-        let arrEl = dom.window.document.querySelectorAll(".tab");
-        arrEl.forEach((el) => {
-            tabs.push({
-                link: el.getAttribute("data-tab").trim(),
-                value: el.textContent.trim(),
-            });
-        });
-
-        tabs.forEach((el) => {
-            const elem = dom.window.document.querySelector(`#${el.link}`);
-            const items = [];
-
-            elem.querySelectorAll("tbody").forEach((item) => {
-                item.querySelectorAll("tr").forEach((i) => {
-                    items.push({
-                        type: i.querySelectorAll("td")[0].textContent.trim(),
-                        date: i.querySelectorAll("td")[1].textContent.trim(),
-                        market: i.querySelectorAll("td")[2].textContent.trim(),
-                        sum: i.querySelectorAll("td")[3].textContent.trim(),
-                        change: i.querySelectorAll("td")[4].textContent.trim(),
-                        time: i.querySelectorAll("td")[5].textContent.trim(),
-                        score: i.querySelectorAll("td")[6].textContent.trim(),
-                        odd: i.querySelectorAll("td")[7].textContent.trim(),
-                        percentChange: i
-                            .querySelectorAll("td")[8]
-                            .textContent.trim(),
-                        totalSum: i
-                            .querySelectorAll("td")[9]
-                            .textContent.trim(),
-                        percentOfMarket: i
-                            .querySelectorAll("td")[10]
-                            .textContent.trim(),
-                    });
-                });
-            });
-
-            outcomed.push({
-                market: el.value,
-                values: items,
-            });
-        });
-
-        console.log(outcomed);
+        console.log(response.data);
     } catch (error) {
-        console.log(error);
+        console.error(error);
     }
 };
 
-lineupsSoccer365();
+getData();
