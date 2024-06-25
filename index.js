f5bd4b3ab566ade5c91692dbb0aae390e0f3c462
Kredo <kredik305@gmail.com>
2024-06-26 01:11:42 +0300

    add new endpoint

--- index.js
+++ index.js
@@ -1,40 +1,11 @@
 import express from "express";
 import cors from "cors";
-// import { moneyWay1x2 } from "./services/arbworld/moneyWay1x2.js";
-// import { moneyWayUnderOver } from "./services/arbworld/moneyUnderOver.js";
-// import { correctScore } from "./services/arbworld/correctScore.js";
-// import { onlineBookmaker } from "./services/prediction/onlineBookmaker.js";
-// import { betzona } from "./services/prediction/betzona.js";
-// import { betzonaPredict } from "./services/prediction/betzonaPredict.js";
-// import { getMatchesSoccer365 } from "./services/soccer365/soccer365AllMatches.js";
-// import { matchSoccer365 } from "./services/soccer365/matchSoccer365.js";
-// import { h2hSoccer365 } from "./services/soccer365/h2h.js";
-// import { formSoccer365 } from "./services/soccer365/form.js";
-// import { liveSoccer365 } from "./services/soccer365/live.js";
-// import { lineupsSoccer365 } from "./services/soccer365/lineups.js";
-// import { stavkiprognozy } from "./services/prediction/stavkiprognozy.js";
-// import { stavkiprognozyPredict } from "./services/prediction/stavkiprognozyPredict.js";
-// import { getDroppingOdds1X2 } from "./services/arbworld/droppingOdds1x2.js";
-// import { getDroppingOddsUnderOver } from "./services/arbworld/droppingOddsUO.js";
-// import { euroFootball } from "./services/prediction/euroFootbal.js";
-// import { euroFootballPredict } from "./services/prediction/euroFootballPredict.js";
-// import { liveresult } from "./services/prediction/liveResult.js";
-// import { liveresultPredict } from "./services/prediction/liveresultPredict.js";
-// import { legalbet } from "./services/prediction/legalBet.js";
-// import { leagalbetPredict } from "./services/prediction/legalbetPredict.js";
-// import { sportAndBets } from "./services/prediction/sportAndBets.js";
-// import { sportAndBetsPredict } from "./services/prediction/sportAndBetsPredict.js";
-// import { oddsRu } from "./services/prediction/oddsRu.js";
-// import { oddsRuPredict } from "./services/prediction/oddsRuPredict.js";
+
 import { getMatchesNbBetPrematch } from "./services/nbbet/nbbet.js";
 import { getMatchNbBetPrematch } from "./services/nbbet/nbbetMatch.js";
 import { getMatchesNbBetPredict } from "./services/nbbet/nbbetPredict.js";
-import { getHisoryOdds } from "./services/nbbet/nbBetOdds.js";
-// import { getPredictionsStavkaTV } from "./services/stavkaTV/predictionsStavkaTV.js";
-import { getMatchesNbBetPrematchHockey } from "./services/nbbet/nbBetHockey.js";
-import { getMatchNbBetPrematchHockey } from "./services/nbbet/nbBetMatchHockey.js";
-// import { getAllMatchesExcaper } from "./services/excaper/getAllMatches.js";
-// import { getMatchDataExcaper } from "./services/excaper/getMatch.data.js";
+import { getSummary } from "./services/nbbet/nbetSummary.js";
+
 
 const app = express();
 
@@ -52,24 +23,16 @@ app.get("/nbbetMatches", async (req, res) => {
     const matches = await getMatchesNbBetPrematch(req.query.timestamp);
     res.json({ matches });
 });
-app.get("/nbbetMatchesHockey", async (req, res) => {
-    const matches = await getMatchesNbBetPrematchHockey(req.query.timestamp);
-    res.json({ matches });
-});
 app.get("/nbbetMatch", async (req, res) => {
     const match = await getMatchNbBetPrematch(req.query.link);
-    res.json({ match });
-});
-app.get("/nbbetMatchHockey", async (req, res) => {
-    const match = await getMatchNbBetPrematchHockey(req.query.link);
     res.json({ match });
 });
 app.get("/nbbetPredict", async (req, res) => {
     const match = await getMatchesNbBetPredict(req.query.link);
     res.json({ match });
 });
-app.get("/nbbetOdds", async (req, res) => {
-    const match = await getHisoryOdds(req.query.link);
+app.get("/nbbetSummary", async (req, res) => {
+    const match = await getSummary(req.query.link);
     res.json({ match });
 });
 
